7dc9b372e1e844e71519aea4d60d59347194ea3a
mld36911 <mld36911@gmail.com>
2026-05-22 22:03:40 -0400

    add findActiveMembership

--- src/main/java/com/mldtech/nilapp/api/users/children/UserEntity/repository/UserEntityRepository.java
+++ src/main/java/com/mldtech/nilapp/api/users/children/UserEntity/repository/UserEntityRepository.java
@@ -3,6 +3,7 @@ package com.mldtech.nilapp.api.users.children.UserEntity.repository;
 
 import com.mldtech.nilapp.api.users.children.UserEntity.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -13,7 +14,8 @@ public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
 
     void deleteByUserUserIdAndEntityEntityId(Long userId, Long entityId);
     List<UserEntity> findByUser_UserId(Long userId);
-
+    @Query("SELECT ue FROM UserEntity ue WHERE ue.user.userId = :userId AND ue.entity.entityId = :entityId AND ue.leftAt IS NULL")
+    UserEntity findActiveMembership(Long userId, Long entityId);
 
 }
 
--- src/main/java/com/mldtech/nilapp/api/users/children/UserGroup/repository/UserGroupRepository.java
+++ src/main/java/com/mldtech/nilapp/api/users/children/UserGroup/repository/UserGroupRepository.java
@@ -2,12 +2,16 @@ package com.mldtech.nilapp.api.users.children.UserGroup.repository;
 
 import com.mldtech.nilapp.api.users.children.UserGroup.model.UserGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface UserGroupRepository extends JpaRepository<UserGroup, Long> {
 //    List<UserGroup> findByUserGroupUserId(Long userId);
     List<UserGroup> findByUser_UserId(Long userId);
+
+    @Query("SELECT ug FROM UserGroup ug WHERE ug.user.userId = :userId AND ug.group.groupId = :groupId AND ug.leftAt IS NULL")
+    UserGroup findActiveMembership(Long userId, Long groupId);
 
 
 }
